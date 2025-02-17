7d6edac3d3d2db5938d93fb1cd7dc14ca2dcb111
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-02-17 13:44:39 +0600

    frontend working place Order Page

--- backend/routers/orderRoute.js
+++ backend/routers/orderRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import authMiddleware from "../middleware/auth"
+import authMiddleware from "../middleware/auth.js"
 import { placeOrder } from "../controllers/orderController.js"
 
 
