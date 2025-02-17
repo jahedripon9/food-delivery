fe664ef0b0e0aff280992ae47b4cff15accb017a
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-02-17 12:16:40 +0600

    Order Model, Order Controller & Payment Stripe Setup

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { connectDB } from './config/db.js';
 import foodRoute from './routers/foodRoute.js';
 import userRouter from './routers/userRoute.js';
 import cartRouter from './routers/cartRoute.js';
+import orderRouter from './routers/orderRoute.js';
 
 // app config
 const app = express();
@@ -23,6 +24,7 @@ app.use("/api/food", foodRoute);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter)
 
 
 app.get('/', (req, res) => {
