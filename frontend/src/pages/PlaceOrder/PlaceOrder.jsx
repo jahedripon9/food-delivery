92f4c0e2b446860c7d35ea3569b22cb021e73f68
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-02-23 13:02:15 +0600

    My Order Perfectly Working Backend & Frontend

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,5 +1,6 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
+import { useNavigate } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios'
 
@@ -50,6 +51,18 @@ const PlaceOrder = () => {
         }
     }
 
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!token) {
+            navigate('/cart')
+        }
+        else if (
+            getTotalCartAmount() === 0
+        ) {
+            navigate('/cart')
+        }
+    }, [token])
     return (
         <form onSubmit={placeOrder} className='place-order'>
             <div className="place-order-left">
