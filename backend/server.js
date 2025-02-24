b417fac60d9828b08f0d708fa507c2542a3c80f8
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-02-24 11:54:35 +0600

    Ready Full Stack Delivery Website in Reactjs, MongoDB, Express, Nodejs & Stripe

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,32 @@
 import express from 'express';
 import cors from 'cors';
-import { connect } from 'mongoose';
-import 'dotenv/config'
 import { connectDB } from './config/db.js';
 import foodRoute from './routers/foodRoute.js';
 import userRouter from './routers/userRoute.js';
 import cartRouter from './routers/cartRoute.js';
 import orderRouter from './routers/orderRoute.js';
+import { createServer } from '@vercel/node';  // Vercel-Compatible Handler
 
-// app config
+// App Config
 const app = express();
-const port = 4000;
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-// db connection
+// DB Connection
 connectDB();
 
-// API endpoints
+// API Endpoints
 app.use("/api/food", foodRoute);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter)
-
+app.use("/api/order", orderRouter);
 
 app.get('/', (req, res) => {
     res.send("API is running...");
-})
+});
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
-
+// Export the Vercel serverless handler
+export default createServer(app);
