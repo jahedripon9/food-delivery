3e9105d859c03122dc37f6e317dfcec0c7694d59
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-02-24 11:11:06 +0600

    Ready Full Stack Delivery Website in Reactjs, MongoDB, Express, Nodejs & Stripe

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -20,6 +20,17 @@ export const Orders = ({ url }) => {
             toast.error("Error")
         }
     }
+
+    const statusHandler = async (event, orderId) => {
+        const response = await axios.post(url + "/api/order/status", {
+            orderId,
+            status: event.target.value
+        })
+        if (response.data.success) {
+            await fetchAllOrders()
+        }
+    }
+
     useEffect(() => {
         fetchAllOrders()
     }, [])
@@ -54,7 +65,7 @@ export const Orders = ({ url }) => {
                             </div>
                             <p className=''>Items : {order.items.length}</p>
                             <p>${order.amount}</p>
-                            <select >
+                            <select onChange={(event) => statusHandler(event, order._id)} value={order.status}>
                                 <option value="Food Processing">Food Processing</option>
                                 <option value="Out for Delivery">Out for Delivery</option>
                                 <option value="Delivered">Delivered</option>
