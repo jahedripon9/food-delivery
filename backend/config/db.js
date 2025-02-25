4e14e25ffbaf148f35555dc25154b7940a2a2732
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-02-25 11:03:45 +0600

    Update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,3 +3,4 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     await mongoose.connect('mongodb+srv://food-del:hello121@cluster0.uugjr.mongodb.net/food-del').then(() => console.log('DB Connected'));
 }
+
